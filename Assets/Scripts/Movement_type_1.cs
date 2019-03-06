46ed6ea87d98408f0cc68926624859a3e092c101
AlexandreSolovieff <48060812+AlexandreSolovieff@users.noreply.github.com>
2019-03-06 18:46:26 +0100

    Scripts de mouvement + white pixel

---
+++ Assets/Scripts/Movement_type_1.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement_type_1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(Vector2.right * 4f * Time.deltaTime);
+            transform.eulerAngles = new Vector3(0, 0, 0);
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(Vector2.right * 4f * Time.deltaTime);
+            transform.eulerAngles = new Vector3(0, 0, 180);
+        }
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(Vector2.right * 4f * Time.deltaTime);
+            transform.eulerAngles = new Vector3(0, 0, 90);
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(Vector2.right * 4f * Time.deltaTime);
+            transform.eulerAngles = new Vector3(0, 0, -90);
+        }
+    }
+}
---
+++ Assets/Scripts/Movement_type_2.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement_type_2 : MonoBehaviour
+{
+    private float norm = 0.5f; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+        Move();
+        
+        
+    }
+
+    void Move()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(Vector2.right * norm);
+            transform.eulerAngles = new Vector3(0, 0, 0);
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(Vector2.right * norm);
+            transform.eulerAngles = new Vector3(0, 0, 180);
+        }
+        else if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(Vector2.right * norm);
+            transform.eulerAngles = new Vector3(0, 0, 90);
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(Vector2.right * norm);
+            transform.eulerAngles = new Vector3(0, 0, -90);
+        }
+    }
+}
