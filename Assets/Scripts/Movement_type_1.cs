5e97d3e4acefcd0bf1d0e6343546cb16292f6db8
FelixPierre <44431537+FelixPierre@users.noreply.github.com>
2019-03-20 00:24:03 +0100

    animation du player + prefab de map vide

--- Assets/Scripts/Movement_type_1.cs
+++ Assets/Scripts/Movement_type_1.cs
@@ -5,11 +5,12 @@ using UnityEngine;
 public class Movement_type_1 : MonoBehaviour
 {
     private float speed = 0.1f;
+    private Animator animator;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -30,8 +31,10 @@ public class Movement_type_1 : MonoBehaviour
     {
         float horizontal_mvt = Input.GetAxis("Horizontal");
         float vertical_mvt = Input.GetAxis("Vertical");
-        
+
+        animator.SetFloat("HorizontalSpeed", horizontal_mvt);
+        animator.SetFloat("VerticalSpeed", vertical_mvt);
+
         GetComponent<Rigidbody2D>().MovePosition(transform.position + (Vector3.right * horizontal_mvt + Vector3.up * vertical_mvt) * speed);
-            
     }
 }
