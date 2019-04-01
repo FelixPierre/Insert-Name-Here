59db2cc9421a5d573b09997de83d237ae6ed0d74
Alexandre Solovieff <asolovieff@hotmail.fr>
2019-04-02 01:02:15 +0200

    Main Menu
    Beginning of a main menu: a non operational option menu is available in addition of the quitting button.

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuController : MonoBehaviour
+{
+    public GameObject mainMenu;
+    public GameObject optionsMenu;
+
+    public Slider soundVolumeButton;
+    public Button optionsButton;
+
+    public void OpenOptions()
+    {
+        mainMenu.SetActive(false);
+        optionsMenu.SetActive(true);
+        soundVolumeButton.Select();
+    }
+
+    public void CloseOptions()
+    {
+        optionsMenu.SetActive(false);
+        mainMenu.SetActive(true);
+        optionsButton.Select();
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
