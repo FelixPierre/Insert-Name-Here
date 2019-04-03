e71c29713f728e5ca0298c21e908fddac8c60cdb
Alexandre Solovieff <asolovieff@hotmail.fr>
2019-04-03 20:07:47 +0200

    MainMenu and GameMenu
    Pause of the creation of the MainMenu and beginning of a GameMenue.

---
+++ Assets/Scripts/GameMenuControler.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMenuControler : MonoBehaviour
+{
+    public GameObject gameMenu;
+
+    private bool isOpen = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isOpen)
+            {
+                closeGameMenu();
+            }
+            else
+            {
+                openGameMenu();            
+            }
+        }
+    }
+
+    void openGameMenu()
+    {
+        Time.timeScale = 0;
+        gameMenu.SetActive(true);
+        isOpen = true;
+    }
+
+    void closeGameMenu()
+    {
+        gameMenu.SetActive(false);
+        Time.timeScale = 1;
+        isOpen = false;
+    }
+}
